57468b862aaeeb83c22c6d92a998e9a4fe35539e
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2020-09-02 10:48:01 +0200

    Add blog example

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,37 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+require('dotenv').config({
+  path: `.env`,
+})
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Gatsby`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: ['.mdx', '.md'],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    'gatsby-remark-images',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/content/posts`,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data)
   return <div>Hello world!</div>
 }
+
+export const query = graphql`
+  {
+    allPosts: allMdx {
+      edges {
+        node {
+          ...productFields
+        }
+      }
+    }
+  }
+`
+
+export const fragment = graphql`
+  fragment productFields on Mdx {
+    id
+    frontmatter {
+      title
+      coverImage {
+        id
+        name
+      }
+    }
+  }
+`
